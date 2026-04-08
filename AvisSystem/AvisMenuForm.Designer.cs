569995f849b9ce5411ba3714e56b3dbb989c5f10
Nandipha <Nandipha@PC>
2026-04-08 23:15:37 +0200

    Add project files.

---
+++ AvisSystem/AvisMenuForm.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace AvisSystem
+{
+    partial class AvisMenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.manageCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewActiveCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateCustomersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateCustomersToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageReservationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewScheduledReservationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addReservationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateReservationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageRentalsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addRentalsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateRentalsInformationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageVehiclesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewVehiclesStatusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateVehicleStatusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processPaymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewPaymentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPaymentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updatePaymentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageCustomerToolStripMenuItem,
+            this.manageReservationToolStripMenuItem,
+            this.manageRentalsToolStripMenuItem,
+            this.manageVehiclesToolStripMenuItem,
+            this.processPaymentToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // manageCustomerToolStripMenuItem
+            // 
+            this.manageCustomerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewActiveCustomerToolStripMenuItem,
+            this.updateCustomersToolStripMenuItem,
+            this.updateCustomersToolStripMenuItem1});
+            this.manageCustomerToolStripMenuItem.Name = "manageCustomerToolStripMenuItem";
+            this.manageCustomerToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
+            this.manageCustomerToolStripMenuItem.Text = "Manage Customer";
+            // 
+            // viewActiveCustomerToolStripMenuItem
+            // 
+            this.viewActiveCustomerToolStripMenuItem.Name = "viewActiveCustomerToolStripMenuItem";
+            this.viewActiveCustomerToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.viewActiveCustomerToolStripMenuItem.Text = "View Active Customer";
+            // 
+            // updateCustomersToolStripMenuItem
+            // 
+            this.updateCustomersToolStripMenuItem.Name = "updateCustomersToolStripMenuItem";
+            this.updateCustomersToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.updateCustomersToolStripMenuItem.Text = "Add  Customers";
+            // 
+            // updateCustomersToolStripMenuItem1
+            // 
+            this.updateCustomersToolStripMenuItem1.Name = "updateCustomersToolStripMenuItem1";
+            this.updateCustomersToolStripMenuItem1.Size = new System.Drawing.Size(238, 22);
+            this.updateCustomersToolStripMenuItem1.Text = "Update Customers Information";
+            // 
+            // manageReservationToolStripMenuItem
+            // 
+            this.manageReservationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewScheduledReservationToolStripMenuItem,
+            this.addReservationToolStripMenuItem,
+            this.updateReservationToolStripMenuItem});
+            this.manageReservationToolStripMenuItem.Name = "manageReservationToolStripMenuItem";
+            this.manageReservationToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.manageReservationToolStripMenuItem.Text = "Manage Reservation";
+            // 
+            // viewScheduledReservationToolStripMenuItem
+            // 
+            this.viewScheduledReservationToolStripMenuItem.Name = "viewScheduledReservationToolStripMenuItem";
+            this.viewScheduledReservationToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.viewScheduledReservationToolStripMenuItem.Text = "View Scheduled Reservation";
+            // 
+            // addReservationToolStripMenuItem
+            // 
+            this.addReservationToolStripMenuItem.Name = "addReservationToolStripMenuItem";
+            this.addReservationToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.addReservationToolStripMenuItem.Text = "Add Reservation";
+            // 
+            // updateReservationToolStripMenuItem
+            // 
+            this.updateReservationToolStripMenuItem.Name = "updateReservationToolStripMenuItem";
+            this.updateReservationToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.updateReservationToolStripMenuItem.Text = "Update Reservation";
+            // 
+            // manageRentalsToolStripMenuItem
+            // 
+            this.manageRentalsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem,
+            this.addRentalsToolStripMenuItem,
+            this.updateRentalsInformationToolStripMenuItem});
+            this.manageRentalsToolStripMenuItem.Name = "manageRentalsToolStripMenuItem";
+            this.manageRentalsToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.manageRentalsToolStripMenuItem.Text = "Manage Rentals";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.viewToolStripMenuItem.Text = "View Rentals";
+            // 
+            // addRentalsToolStripMenuItem
+            // 
+            this.addRentalsToolStripMenuItem.Name = "addRentalsToolStripMenuItem";
+            this.addRentalsToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.addRentalsToolStripMenuItem.Text = "Add Rentals";
+            // 
+            // updateRentalsInformationToolStripMenuItem
+            // 
+            this.updateRentalsInformationToolStripMenuItem.Name = "updateRentalsInformationToolStripMenuItem";
+            this.updateRentalsInformationToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.updateRentalsInformationToolStripMenuItem.Text = "Update Rentals Information";
+            // 
+            // manageVehiclesToolStripMenuItem
+            // 
+            this.manageVehiclesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewVehiclesStatusToolStripMenuItem,
+            this.updateVehicleStatusToolStripMenuItem});
+            this.manageVehiclesToolStripMenuItem.Name = "manageVehiclesToolStripMenuItem";
+            this.manageVehiclesToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.manageVehiclesToolStripMenuItem.Text = "Manage Vehicles";
+            // 
+            // viewVehiclesStatusToolStripMenuItem
+            // 
+            this.viewVehiclesStatusToolStripMenuItem.Name = "viewVehiclesStatusToolStripMenuItem";
+            this.viewVehiclesStatusToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.viewVehiclesStatusToolStripMenuItem.Text = "View Vehicles Status";
+            // 
+            // updateVehicleStatusToolStripMenuItem
+            // 
+            this.updateVehicleStatusToolStripMenuItem.Name = "updateVehicleStatusToolStripMenuItem";
+            this.updateVehicleStatusToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.updateVehicleStatusToolStripMenuItem.Text = "Update Vehicle Status";
+            // 
+            // processPaymentToolStripMenuItem
+            // 
+            this.processPaymentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewPaymentsToolStripMenuItem,
+            this.addPaymentToolStripMenuItem,
+            this.updatePaymentsToolStripMenuItem});
+            this.processPaymentToolStripMenuItem.Name = "processPaymentToolStripMenuItem";
+            this.processPaymentToolStripMenuItem.Size = new System.Drawing.Size(109, 20);
+            this.processPaymentToolStripMenuItem.Text = "Process Payment";
+            // 
+            // viewPaymentsToolStripMenuItem
+            // 
+            this.viewPaymentsToolStripMenuItem.Name = "viewPaymentsToolStripMenuItem";
+            this.viewPaymentsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.viewPaymentsToolStripMenuItem.Text = "View Payments";
+            // 
+            // addPaymentToolStripMenuItem
+            // 
+            this.addPaymentToolStripMenuItem.Name = "addPaymentToolStripMenuItem";
+            this.addPaymentToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addPaymentToolStripMenuItem.Text = "Add Payment";
+            // 
+            // updatePaymentsToolStripMenuItem
+            // 
+            this.updatePaymentsToolStripMenuItem.Name = "updatePaymentsToolStripMenuItem";
+            this.updatePaymentsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.updatePaymentsToolStripMenuItem.Text = "Update payments";
+            // 
+            // AvisMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "AvisMenuForm";
+            this.Text = "AvisMenuForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem manageCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewActiveCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateCustomersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateCustomersToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem manageReservationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewScheduledReservationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addReservationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateReservationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageRentalsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addRentalsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateRentalsInformationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageVehiclesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewVehiclesStatusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateVehicleStatusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processPaymentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewPaymentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addPaymentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updatePaymentsToolStripMenuItem;
+    }
+}
---
+++ AvisSystem/ManageCustomers.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace AvisSystem
+{
+    partial class ManageCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.manageCustomersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateCustomerInformationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageCustomersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // manageCustomersToolStripMenuItem
+            // 
+            this.manageCustomersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewCustomerToolStripMenuItem,
+            this.updateCustomerInformationToolStripMenuItem});
+            this.manageCustomersToolStripMenuItem.Name = "manageCustomersToolStripMenuItem";
+            this.manageCustomersToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.manageCustomersToolStripMenuItem.Text = "Manage Customers";
+            // 
+            // addNewCustomerToolStripMenuItem
+            // 
+            this.addNewCustomerToolStripMenuItem.Name = "addNewCustomerToolStripMenuItem";
+            this.addNewCustomerToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.addNewCustomerToolStripMenuItem.Text = "Add Customer";
+            // 
+            // updateCustomerInformationToolStripMenuItem
+            // 
+            this.updateCustomerInformationToolStripMenuItem.Name = "updateCustomerInformationToolStripMenuItem";
+            this.updateCustomerInformationToolStripMenuItem.Size = new System.Drawing.Size(233, 22);
+            this.updateCustomerInformationToolStripMenuItem.Text = "Update Customer Information";
+            // 
+            // ManageCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ManageCustomers";
+            this.Text = "ManageCustomers";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem manageCustomersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateCustomerInformationToolStripMenuItem;
+    }
+}
