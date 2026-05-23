3e042497bd0092e2d337091c8205fa6f0d9c58ca
Nandipha663 <ntanzinandipha7@gmail.com>
2026-05-23 14:26:44 +0200

    Design Modified

--- AvisSystem/UpdateRental.Designer.cs
+++ AvisSystem/UpdateRental.Designer.cs
@@ -297,7 +297,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(116, 32);
+            this.textBox2.Location = new System.Drawing.Point(110, 28);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(291, 20);
             this.textBox2.TabIndex = 6;
